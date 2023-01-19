1711d7643549557b478756a6a4ca9ff578c0752a
Hechen Liu <codingchallenge4@gmail.com>
2023-01-19 13:04:25 -0500

    work on scraper

--- python.py
+++ python.py
@@ -2,6 +2,17 @@ import requests
 from lxml.html import document_fromstring
 
 
-r = requests.get("https://www.python.org/downloads/")
+element = requests.get("https://www.python.org/downloads/")
 
-print(document_fromstring(r.text).find_class("download-list-widget"))
+releases = document_fromstring(element.text).find_class("release-number")
+
+#releases = document_fromstring(element.text).find_class("release-download")
+
+for i in range(1,len(releases)):
+   print(releases[i].getchildren()[0].text_content())
+   
+   #https://www.python.org/ftp/python/3.8.12/
+    
+   #print("https://www.python.org" + releases[i].getchildren()[0].get("href"))
+    
+    # [0].get("href")
